79873c3804d6c23fd840bcb8b5613151c55e4079
Bruno Melo <melobrunoti@gmail.com>
2022-01-27 17:00:15 -0300

    setup inicial

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import Drinks from './pages/Drinks';
+import Explore from './pages/Explore';
+import FavoriteRecipes from './pages/FavoriteRecipes';
+import Foods from './pages/Foods';
+import Login from './pages/Login';
+import Profile from './pages/Profile';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route path="/drinks" component={ Drinks } />
+        <Route path="/explore" component={ Explore } />
+        <Route path="/favorites-recipes" component={ FavoriteRecipes } />
+        <Route path="/drinks" component={ Drinks } />
+        <Route path="/foods" component={ Foods } />
+        <Route path="/login" component={ Login } />
+        <Route path="/profile" component={ Profile } />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/FavoriteRecipes.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const favoriteRecipes = () => (
+  <div>
+    <h1>favorites</h1>
+  </div>
+);
+
+export default favoriteRecipes;
