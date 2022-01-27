93e129eebe2b07c5e685291c21972adc09638b8f
Danielle Silva <danielle.luisasilva@gmail.com>
2022-01-27 19:28:34 -0300

    adiciona requsito 12

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,46 +1,46 @@
+// import React, { useState } from 'react';
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
-// class Header extends React.Component {
-//   constructor() {
-//     super();
+class Header extends React.Component {
+  constructor() {
+    super();
 
-//     this.state = {
-//       searchVisible: true,
-//     };
-//   }
+    this.state = {
+      isSearchVisible: false,
+    };
+  }
 
-//   render() {
-//     const { pageName } = this.props;
-//     return (
-//       <div>
-//         <Link to="/profile">
-//           <img data-testid="profile-top-btn" src={ profileIcon } alt="profile icon" />
-//         </Link>
-//         <p data-testid="page-title">{ pageName }</p>
-//         <img data-testid="search-top-btn" src={ searchIcon } alt="search icon" />
-//       </div>
-//     );
-//   }
-// }
+  handleClick = () => {
+    const { isSearchVisible } = this.state;
+    this.setState({
+      isSearchVisible: !isSearchVisible,
+    });
+  }
 
-function Header({ pageName, searchVisible }) {
-  console.log(searchVisible);
-  return (
-    <div>
-      <Link to="/profile">
-        <img data-testid="profile-top-btn" src={ profileIcon } alt="profile icon" />
-      </Link>
-      <p data-testid="page-title">{ pageName }</p>
-      {searchVisible
-        ? <img data-testid="search-top-btn" src={ searchIcon } alt="search icon" />
-        : null}
+  render() {
+    const { pageName, searchVisible } = this.props;
+    const { isSearchVisible } = this.state;
+    return (
+      <div>
+        <Link to="/profile">
+          <img data-testid="profile-top-btn" src={ profileIcon } alt="profile icon" />
+        </Link>
+        <p data-testid="page-title">{ pageName }</p>
+        {searchVisible ? (
+          <button type="button" onClick={ this.handleClick }>
+            <img data-testid="search-top-btn" src={ searchIcon } alt="search icon" />
+          </button>)
+          : null}
 
-    </div>
-  );
+        {isSearchVisible ? <input type="text" data-testid="search-input" /> : null}
+
+      </div>
+    );
+  }
 }
 
 Header.propTypes = {
