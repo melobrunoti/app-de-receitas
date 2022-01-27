99128a8fe24ea474d71737307263fc626960a4c3
Danielle Silva <danielle.luisasilva@gmail.com>
2022-01-27 20:34:35 -0300

    adiciona rotas do requisito 9

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import ExploreFoods from './pages/ExploreFoods';
 import ExploreDrinks from './pages/ExploreDrinks';
 import ExploreDrinksIngredients from './pages/ExploreDrinksIngredients';
 import ExploreFoodIngredients from './pages/ExploreFoodIngredients';
+import ExploreFoodsNationality from './pages/ExplorFoodsNationality';
 
 function App() {
   return (
@@ -21,19 +22,27 @@ function App() {
       <Switch>
         <Route exact path="/" component={ Login } />
         <Route path="/drinks" component={ Drinks } />
-        <Route path="/explore" component={ Explore } />
-        <Route path="/explore/foods" component={ ExploreFoods } />
-        <Route path="/explore/drinks" component={ ExploreDrinks } />
-        <Route path="/explore/foods/ingredients" component={ ExploreFoodIngredients } />
+        <Route exact path="/explore" component={ Explore } />
+        <Route exact path="/explore/foods" component={ ExploreFoods } />
+        <Route exact path="/explore/drinks" component={ ExploreDrinks } />
         <Route
+          exact
+          path="/explore/foods/ingredients"
+          component={ ExploreFoodIngredients }
+        />
+        <Route
+          exact
           path="/explore/drinks/ingredients"
           component={ ExploreDrinksIngredients }
         />
+        <Route
+          exact
+          path="/explore/foods/nationalities"
+          component={ ExploreFoodsNationality }
+        />
         <Route path="/favorite-recipes" component={ FavoriteRecipes } />
-        <Route path="/drinks" component={ Drinks } />
         <Route path="/done-recipes" component={ DoneRecipes } />
         <Route path="/foods" component={ Foods } />
-        <Route path="/login" component={ Login } />
         <Route path="/profile" component={ Profile } />
       </Switch>
     </BrowserRouter>
--- src/pages/ExploreFoods.js
+++ src/pages/ExploreFoods.js
@@ -3,7 +3,7 @@ import Header from '../components/Header';
 
 const ExploreFoods = () => (
   <div>
-    <Header pageName="Explore Drinks" searchVisible={ false } />
+    <Header pageName="Explore Foods" searchVisible={ false } />
     <h1>Explore Foods</h1>
   </div>);
 
