a0aca81ea10003ba7fce41e3e7665bfd691507cf
Alissandra <alissandraraysa@gmail.com>
2022-01-27 22:47:47 -0300

    requisitos 2, 3 e 4

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,6 +3,28 @@ import React from 'react';
 const login = () => (
   <div>
     <h1>LOGIN</h1>
+    <form>
+      <input
+        type="email"
+        name="email"
+        placeholder="Email"
+        id="email"
+        data-testid="email-input"
+      />
+      <input
+        type="password"
+        name="password"
+        placeholder="Password"
+        id="Password"
+        data-testid="password-input"
+      />
+      <button
+        type="button"
+        data-testid="login-submit-btn"
+      >
+        Enter
+      </button>
+    </form>
   </div>);
 
 export default login;
