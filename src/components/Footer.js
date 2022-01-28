d016b3c9111018a77000ec8baa00b198b8a47fea
Lucca Neiva <luccaneiva12@gmail.com>
2022-01-28 01:03:03 -0300

    Requisito 20 concluido

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
+import drinkIcon from '../images/drinkIcon.svg';
+import exploreIcon from '../images/exploreIcon.svg';
+import foodIcon from '../images/mealIcon.svg';
+
+import './Footer.css';
+
 function Footer() {
   return (
     <footer data-testid="footer">
-      <button type="button" data-testid="drinks-bottom-btn">DRINKS</button>
-      <button type="button" data-testid="explore-bottom-btn">EXPLORE</button>
-      <button type="button" data-testid="food-bottom-btn">FOODS</button>
+      <button type="button" data-testid="drinks-bottom-btn" title="Drinks">
+        <img src={ drinkIcon } alt="drinks button" />
+      </button>
+      <button type="button" data-testid="explore-bottom-btn" title="Explore">
+        <img src={ exploreIcon } alt="explore button" />
+      </button>
+      <button type="button" data-testid="food-bottom-btn" title="Foods">
+        <img src={ foodIcon } alt="foods button" />
+      </button>
     </footer>
   );
 }
--- src/pages/Foods.js
+++ src/pages/Foods.js
@@ -4,6 +4,41 @@ import Footer from '../components/Footer';
 const foods = () => (
   <div>
     <h1>FOODS</h1>
+    a
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    a
+    a
+    a
+    a
+    a
+    a
+    a
+    a
+    aa
+    a
+    a
+    a
     <Footer />
   </div>
 );
