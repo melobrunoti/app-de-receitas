19154a45516a415de6dd260940ab18f1bb7d444b
Danielle Silva <danielle.luisasilva@gmail.com>
2022-01-29 00:01:43 -0300

    adiciona requisitos 83 e 84

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,17 +2,21 @@ import React from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
-const Profile = () => (
-  <div>
-    <Header pageName="Profile" searchVisible={ false } />
-    <h1>Profile</h1>
+function Profile() {
+  const userEmail = JSON.parse(localStorage.getItem('user')).email;
 
-    <p data-testid="profile-email">email</p>
-    <button type="button" data-testid="profile-done-btn">Done Recipes</button>
-    <button type="button" data-testid="profile-favorite-btn">Favorite Recipes</button>
-    <button type="button" data-testid="profile-logout-btn">Logout</button>
-    <Footer />
-  </div>
-);
+  return (
+    <div>
+      <Header pageName="Profile" searchVisible={ false } />
+      <h1>Profile</h1>
+
+      <p data-testid="profile-email">{userEmail}</p>
+      <button type="button" data-testid="profile-done-btn">Done Recipes</button>
+      <button type="button" data-testid="profile-favorite-btn">Favorite Recipes</button>
+      <button type="button" data-testid="profile-logout-btn">Logout</button>
+      <Footer />
+    </div>
+  );
+}
 
 export default Profile;
