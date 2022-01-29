600163d9b927aeee3a24c8200a469008ddb87fed
Danielle Silva <danielle.luisasilva@gmail.com>
2022-01-29 00:11:15 -0300

    adiciona requisitos 85 e 86

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function Profile() {
   const userEmail = JSON.parse(localStorage.getItem('user')).email;
+  const history = useHistory();
+
+  const redirectDoneRecipes = () => {
+    history.push('/done-recipes');
+  };
+
+  const redirectFavoriteRecipes = () => {
+    history.push('/favorite-recipes');
+  };
 
   return (
     <div>
@@ -11,9 +21,31 @@ function Profile() {
       <h1>Profile</h1>
 
       <p data-testid="profile-email">{userEmail}</p>
-      <button type="button" data-testid="profile-done-btn">Done Recipes</button>
-      <button type="button" data-testid="profile-favorite-btn">Favorite Recipes</button>
-      <button type="button" data-testid="profile-logout-btn">Logout</button>
+
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+        onClick={ redirectDoneRecipes }
+      >
+        Done Recipes
+      </button>
+
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+        onClick={ redirectFavoriteRecipes }
+      >
+        Favorite Recipes
+      </button>
+
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+        // onClick={ logout }
+      >
+        Logout
+
+      </button>
       <Footer />
     </div>
   );
