8dc19b9358bcc29e5e045e9231035ab04bd31308
Bruno Melo <melobrunoti@gmail.com>
2022-02-02 16:57:48 -0300

    in progress

--- src/pages/InProgressDrinks.js
+++ src/pages/InProgressDrinks.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useLocation, useParams } from 'react-router-dom';
+import InProgressDrinkCard from '../components/InProgressDrinkCard';
+import { fetchById } from '../services/api';
 
 function InProgressDrinks() {
+  const { id } = useParams();
+  const { pathname } = useLocation();
+  const [inProgressFood, setInProgressFood] = useState();
+  const ingredients = JSON.parse(localStorage.getItem('inProgressRecipes')).cocktails[id];
+
+  useEffect(() => {
+    (async () => {
+      const results = await fetchById(id, pathname);
+      return setInProgressFood(results);
+    })();
+  }, [id, pathname, setInProgressFood]);
+
   return (
     <div>
-      <h1>In Progress Drinks</h1>
+      {(inProgressFood) && <InProgressDrinkCard
+        ingredients={ ingredients }
+        card={ inProgressFood }
+      />}
     </div>
   );
 }
