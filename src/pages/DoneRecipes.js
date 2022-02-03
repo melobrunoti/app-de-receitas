c094db20effa4135950d24e14928cafed382bed6
Danielle Silva <danielle.luisasilva@gmail.com>
2022-02-02 22:57:02 -0300

    remove "mock" do storage

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import DoneRecipeCard from '../components/DoneRecipeCard';
 
 import Header from '../components/Header';
@@ -7,33 +7,6 @@ function DoneRecipes() {
   const originalStorage = JSON.parse(localStorage.getItem('doneRecipes'));
   const [storage, setStorage] = useState(JSON.parse(localStorage.getItem('doneRecipes')));
   console.log(storage);
-
-  const favoritess = [{
-    id: '52771',
-    type: 'food',
-    nationality: 'Italian',
-    category: 'Vegetarian',
-    alcoholicOrNot: '',
-    name: 'Spicy Arrabiata Penne',
-    image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
-    doneDate: '23/06/2020',
-    tags: ['Pasta', 'Curry'],
-  },
-  {
-    id: '178319',
-    type: 'drink',
-    nationality: '',
-    category: 'Cocktail',
-    alcoholicOrNot: 'Alcoholic',
-    name: 'Aquamarine',
-    image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
-    doneDate: '23/06/2020',
-    tags: [],
-  }];
-
-  useEffect(() => {
-    localStorage.setItem('doneRecipes', JSON.stringify(favoritess));
-  }, []);
 
   const foodFilter = () => originalStorage
     .filter((recipe) => recipe.type === 'food')
